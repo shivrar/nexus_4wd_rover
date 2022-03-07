95860f0429b145240bd3bd8a024a9b025366425b
shivrar <s_ram_dhanie@hotmail.com>
2022-03-07 18:46:50 +0000

    Added regulation loop

--- src/main.cpp
+++ src/main.cpp
@@ -21,13 +21,20 @@ MotorWheel wheelLL(11,12,14,15,&irq2);
 irqISR(irq3,isr3);
 MotorWheel wheelLR(9,8,16,17,&irq3);
 irqISR(irq4,isr4);
-MotorWheel wheel4(10,7,18,19,&irq4);
-Omni4WD Omni(&wheelUL,&wheelLL,&wheelLR,&wheel4);
+MotorWheel wheelUR(10,7,18,19,&irq4);
+Omni4WD Omni(&wheelUL,&wheelLL,&wheelLR,&wheelUR);
 
 
 //Tasks are defined as such
 void callBack1(Task* me);
 Task t1(1000, callBack1);
+
+void RegulationCallback(Task* me);
+Task pid_reg(25, RegulationCallback);
+
+void SpeedCheck(Task* me);
+Task speed_check(500, SpeedCheck);
+
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -38,10 +45,23 @@ void setup() {
   TCCR1B=TCCR1B&0xf8|0x01;    // Pin9,Pin10 PWM 31250Hz
   TCCR2B=TCCR2B&0xf8|0x01;    // Pin3,Pin11 PWM 31250Hz
 
-  Omni.PIDEnable(0.31,0.01,0,10);
+  wheelUL.PIDEnable(0.31,0.01,0,10);
+  wheelLL.PIDEnable(0.31,0.01,0,10);
+  wheelLR.PIDEnable(0.31,0.01,0,10);
+  wheelUR.PIDEnable(0.31,0.01,0,10);
 
+//  wheelUL.setSpeedMMPS(30);
+//  wheelLL.setSpeedMMPS(30);
+//  wheelLR.setSpeedMMPS(-30);
+//  wheelUR.setSpeedMMPS(-30);
+  wheelUL.setGearedSpeedRPM(9.57);
+  wheelLL.setGearedSpeedRPM(9.57);
+  wheelLR.setGearedSpeedRPM(-9.57);
+  wheelUR.setGearedSpeedRPM(-9.57);
   // let's add a task to the timer
   SoftTimer.add(&t1);
+  SoftTimer.add(&pid_reg);
+  SoftTimer.add(&speed_check);
 
 }
 
@@ -57,4 +77,19 @@ void loop()
 
 void callBack1(Task* me) {
   digitalWrite(13, !digitalRead(13));
+}
+
+void RegulationCallback(Task* me){
+  //Omni.demoActions(30,1500,500,false);
+  wheelUL.PIDRegulate();
+  wheelLL.PIDRegulate();
+  wheelLR.PIDRegulate();
+  wheelUR.PIDRegulate();
+}
+
+void SpeedCheck(Task* me){
+  Serial.print("speedRPM> ");
+  Serial.println(wheelUL.getSpeedRPM(),DEC);
+  Serial.print("MMPS--> "); //display the speed of the MotorWheel
+  Serial.println(wheelUL.getSpeedMMPS(),DEC); //display the speed of the motor
 }
